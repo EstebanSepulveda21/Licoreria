5ff9bc78e3af018457b03c3638b31d2fbb241a4f
EstebanSepulveda21 <crisylyon@gmail.com>
2021-04-10 15:05:34 -0500

    Base del proyecto

---
+++ src/test/java/com/licoreria/licoreria/LicoreriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licoreria.licoreria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicoreriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
